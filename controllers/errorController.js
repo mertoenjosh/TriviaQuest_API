3f4d0eb0cc2edbc741ccba0342eda949d40003fb
Martin Thuo <mertoenjosh3527@gmail.com>
2022-10-09 20:28:25 +0300

    added global error controller

---
+++ controllers/errorController.js
@@ -0,0 +1,72 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+
+  return new AppError(message, 400);
+};
+
+const handleDuplicateQuestion = err => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Question ${value} already exist in the database.`;
+
+  return new AppError(message, 400);
+};
+
+const handleValidationError = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data: ${errors.join('. ')}`;
+
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    // any error that comes fro out error class isOperational: known
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programing or unknown err: don't leak details to production
+    // TODO: Use a better logging method
+    console.error('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: "Something wen't very wrong",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.status = err.status || 'fail';
+  err.statusCode = err.statusCode || 500;
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateQuestion(err);
+    if (err.name === 'ValidationError') error = handleValidationError(err);
+    // TODO: Add more uncaught operational errors
+
+    sendErrorProd(error, res);
+  }
+};
+
+// TODO
+/**
+ * Define errors severity level
+ * Email admin incase of severe error
+ */
