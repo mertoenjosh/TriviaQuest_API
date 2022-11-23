b987baf05a3f0c9bc85a3301a3336d6f3493604d
Martin Thuo <mertoenjosh3527@gmail.com>
2022-11-23 11:02:52 +0300

    > Added security headers with helmet > Limited 1000 request per hour to prevent DDOS > Limited size of body to parse to 10kb > Used mongosanitize to trevent NoSQL injection > Added xss to prevent XSS > Added hpp to prevent parameter pollution

--- app.js
+++ app.js
@@ -1,24 +1,56 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-
 const questionRouter = require('./routes/questionRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // MODDLEWARES
-// Implement CORS
-app.use(cors());
-app.options('*', cors());
+// Set security HTTP headers
+app.use(helmet());
 
+// Devleopment logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same IP to prevent ddos.
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  mesage: 'Too many requests from this IP. Try again in an hour',
+});
+app.use('/api', limiter);
 
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: ['difficulty', 'category', 'tags'],
+  })
+);
+
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
+
+// Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -26,6 +58,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/api/v1/questions', questionRouter);
+app.use('/api/v1/users', userRouter);
 
 // Handle all unimplemented routes
 app.all('*', (req, res, next) => {
